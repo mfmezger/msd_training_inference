1bd7ba2e3701d186a382076c369c2496fc528b03
Marc Fabian Mezger <marc.mezger@googlemail.com>
2021-11-22 16:05:55 +0100

    Updating the extraction and preprocessing script.

--- preprocessing/download_extract.py
+++ preprocessing/download_extract.py
@@ -1,13 +1,99 @@
 import os
 
 import yaml
+import nibabel as nib
 from monai.apps import download_and_extract
+from pathlib import Path
+from torchio.transforms import ZNormalization
 
 
-def convert_to_pt(cfg):
+def preprocessing_ct(image):
+    """Preprocess the CT images."""
+
+
+    pass
+
+def prepocessing_mr(image):
+    """Preprocess the MRI images."""
+
+    # do z-score normalization.
+    # FIXME: is that really the same as Z Score Normalzation?
+    image = ZNormalization(mean=0.0, std=1.0)(image)
+
+    pass
+
+def save_pt(image, label, save_dir):
+    """Save the images and labels as pytorch files."""
+    pass
+
+
+def convert_images(cfg, data_dir, save_dir):
+    """Convert images to pytorch files."""
+    
+    # create lists of the images.
+    label_list = os.listdir(os.path.join( data_dir, "labelsTr"))
+    images_list = os.listdir(os.path.join( data_dir, "imagesTr"))
+    image_list_test = os.listdir(os.path.join( data_dir, "imagesTs"))
+    
+    
+    # do the preprocessing based on the category.
+    ct_list = ["Task03_Liver", "Task06_Lung", "Task07_Pancreas", "Task10_Colon", "Task08_HepaticVessel", "Task09_Spleen"]
+    ct_preprocessing_decision = False
+    if data_dir in ct_list:
+        ct_preprocessing_decision = True
+    
+    # load the volumes from images and labels.
+    for image, label in zip(images_list, label_list):
+        # load the images and labels.
+        image_path = os.path.join(data_dir, "imagesTr", image)
+        label_path = os.path.join(data_dir, "labelsTr", label)
+
+        # load the images and labels.
+        image = nib.load(image_path)
+        label = nib.load(label_path)
+        
+        image = image.get_fdata()
+        image = image.transpose(2, 0, 1)
+    
+        label = label.get_fdata()
+        label = label.transpose(2, 0, 1)
+
+        # choose prepocessing based on the category.
+        if ct_preprocessing_decision:
+            image = preprocessing_ct(image)
+        else:
+            image = preprocessing_mr(image)
+        
+        # save the images and labels as pytorch files.
+        save_pt(image, label, save_dir)
+
+    
+    
+    
+    # create a list of all of the images in the folders
+    
+
+    # start the conversion of training images and training labels.
+
+
+    # save the images in the new file structure
+
+
+    # convert the test images.
+# the 
+
+def prepare_conversion(cfg):
+    """Collect the folders for the creation of the PyTorch files."""
+
 
     root_dir = cfg["data_storage"]["data_location"]
     pt_dir = cfg["data_storage"]["pt_location"]
+
+    # create the root_dir path in the file system.
+    Path(root_dir).mkdir(parents=False, exist_ok=True)
+    Path(pt_dir).mkdir(parents=False, exist_ok=True)
+
+
     # select the folders to convert to pt
     brain_dir = os.path.join(root_dir, "Task01_BrainTumour")
     heart_dir = os.path.join(root_dir, "Task02_Heart")
@@ -25,22 +111,13 @@ def convert_to_pt(cfg):
 
     # create new folders from names in folder list in pt_dir
     for folder in folder_list:
-        os.makedirs(os.path.join(pt_dir, folder))
-    
-    # load the volumes from images
-
-    # create a list of all of the images in the folders
-    brain_images_list = os.listdir(os.path.join( brain_dir, "imagesTr"))
-    brain_label_list = os.listdir(os.path.join( brain_dir, "labelsTr"))
-    brain_image_list_test = os.listdir(os.path.join( brain_dir, "imagesTs"))
-
-    # start the conversion of training images and training labels.
-    
-
-    # save the images in the new file structure
+        Path(os.path.join(pt_dir, folder)).mkdir(parents=False, exist_ok=True)
+        # create a new folder for training and one for testing.
+        Path(os.path.join(pt_dir, folder, "train")).mkdir(parents=False, exist_ok=True)
+        Path(os.path.join(pt_dir, folder, "test")).mkdir(parents=False, exist_ok=True)
 
 
-    # convert the test images.
+
 
 
 
@@ -55,7 +132,11 @@ def main():
     with open("config.yml", "r") as ymlfile:
         cfg = yaml.safe_load(ymlfile)
 
+
+    # TODO create the folders for the data.
+
     root_dir = cfg["data_storage"]["data_location"]
+
 
     # start by downloading the data
 
